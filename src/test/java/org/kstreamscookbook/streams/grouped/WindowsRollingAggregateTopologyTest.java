500959ce070cf02b38c5fb59bd4217ce49bc6d51
Sven Erik Knop <seknop@gmail.com>
2019-12-16 22:38:06 +0100

    New test case for rolling Window.
    Interesting results: a single record can cause multiple emitted records - one for each matching "bucket".

---
+++ src/test/java/org/kstreamscookbook/streams/grouped/WindowsRollingAggregateTopologyTest.java
@@ -0,0 +1,94 @@
+package org.kstreamscookbook.streams.grouped;
+
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.kstream.TimeWindows;
+import org.apache.kafka.streams.processor.LogAndSkipOnInvalidTimestamp;
+import org.apache.kafka.streams.test.ConsumerRecordFactory;
+import org.apache.kafka.streams.test.OutputVerifier;
+import org.junit.jupiter.api.Test;
+import org.kstreamscookbook.TopologyBuilder;
+import org.kstreamscookbook.TopologyTestBase;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Collections;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+public class WindowsRollingAggregateTopologyTest extends TopologyTestBase {
+  public static final String INPUT_TOPIC = "input-topic";
+  public static final String OUTPUT_TOPIC = "output-topic";
+
+  private StringSerializer stringSerializer = new StringSerializer();
+  private StringDeserializer stringDeserializer = new StringDeserializer();
+
+  @Override
+  protected TopologyBuilder withTopologyBuilder() {
+    // Window of 5 min width sliding by 1 min
+    return new WindowedAggregateTopology(INPUT_TOPIC, OUTPUT_TOPIC,
+            TimeWindows.of(Duration.ofMinutes(3)).advanceBy(Duration.ofMinutes(1)));
+  }
+
+  @Override
+  protected Map<String, String> withProperties() {
+    // Use the timestamp of the message for windowing purposes.
+    // The following classes are available to extract this timestamp, they vary on how they handle invalid timestamps:
+    //
+    // FailOnInvalidTimestamp - throws an exception
+    // LogAndSkipOnInvalidTimestamp - logs a warning that the message will be discarded
+    // UsePreviousTimeOnInvalidTimestamp - the latest extracted valid timestamp of the current record's partition
+    return Collections.singletonMap(StreamsConfig.DEFAULT_TIMESTAMP_EXTRACTOR_CLASS_CONFIG, LogAndSkipOnInvalidTimestamp.class.getName());
+  }
+
+  @Test
+  public void testWindowedAggregation() {
+    ConsumerRecordFactory<String, String> factory = new ConsumerRecordFactory<>(stringSerializer, stringSerializer);
+
+    //
+    Instant start = Instant.parse("2019-04-20T10:35:00.00Z");
+    // first window starts here
+    testDriver.pipeInput(factory.create(INPUT_TOPIC, "a", "0", start.toEpochMilli()));
+    testDriver.pipeInput(factory.create(INPUT_TOPIC, "a", "30s", start.plus(30, ChronoUnit.SECONDS).toEpochMilli()));
+    testDriver.pipeInput(factory.create(INPUT_TOPIC, "a", "60s", start.plus(3, ChronoUnit.SECONDS).toEpochMilli()));
+//    // second window starts here
+    testDriver.pipeInput(factory.create(INPUT_TOPIC, "a", "90s", start.plus(90, ChronoUnit.SECONDS).toEpochMilli()));
+//    // late arriving message for first window
+    testDriver.pipeInput(factory.create(INPUT_TOPIC, "a", "45s (late)", start.plus(45, ChronoUnit.SECONDS).toEpochMilli()));
+
+    testDriver.pipeInput(factory.create(INPUT_TOPIC, "a", "120s", start.plus(120, ChronoUnit.SECONDS).toEpochMilli()));
+
+
+    OutputVerifier.compareKeyValue(readNextRecord(), "a", "0");
+    OutputVerifier.compareKeyValue(readNextRecord(), "a", "0");
+    OutputVerifier.compareKeyValue(readNextRecord(), "a", "0");
+    OutputVerifier.compareKeyValue(readNextRecord(), "a", "0,30s");
+    OutputVerifier.compareKeyValue(readNextRecord(), "a", "0,30s");
+    OutputVerifier.compareKeyValue(readNextRecord(), "a", "0,30s");
+    OutputVerifier.compareKeyValue(readNextRecord(), "a", "0,30s,60s");
+    OutputVerifier.compareKeyValue(readNextRecord(), "a", "0,30s,60s");
+    OutputVerifier.compareKeyValue(readNextRecord(), "a", "0,30s,60s");
+
+    OutputVerifier.compareKeyValue(readNextRecord(), "a", "0,30s,60s,90s");
+    OutputVerifier.compareKeyValue(readNextRecord(), "a", "0,30s,60s,90s");
+    OutputVerifier.compareKeyValue(readNextRecord(), "a", "90s");
+
+    OutputVerifier.compareKeyValue(readNextRecord(), "a", "0,30s,60s,45s (late)");
+    OutputVerifier.compareKeyValue(readNextRecord(), "a", "0,30s,60s,90s,45s (late)");
+    OutputVerifier.compareKeyValue(readNextRecord(), "a", "0,30s,60s,90s,45s (late)");
+
+    OutputVerifier.compareKeyValue(readNextRecord(), "a", "0,30s,60s,90s,45s (late),120s");
+    OutputVerifier.compareKeyValue(readNextRecord(), "a", "90s,120s");
+    OutputVerifier.compareKeyValue(readNextRecord(), "a", "120s");
+
+    assertNull(readNextRecord());
+  }
+
+  private ProducerRecord<String, String> readNextRecord() {
+    return testDriver.readOutput(OUTPUT_TOPIC, stringDeserializer, stringDeserializer);
+  }
+}
