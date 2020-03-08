c6f4e82438f1dd52c3fb1fd67f3532c93ac7c935
Sven Erik Knop <seknop@gmail.com>
2020-03-08 15:46:09 +0000

    Upgrade to test framework 2.4

--- src/test/java/org/kstreamscookbook/streams/windowed/TimeWindowedCountTopologyTest.java
+++ src/test/java/org/kstreamscookbook/streams/windowed/TimeWindowedCountTopologyTest.java
@@ -18,14 +18,11 @@ import java.util.Collections;
 import java.util.Map;
 import java.util.function.Supplier;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertNull;
 
 class TimeWindowedCountTopologyTest extends TopologyTestBase {
 
-    public static final String INPUT_TOPIC = "input-topic";
-    public static final String OUTPUT_TOPIC = "output-topic";
-
-    private StringSerializer stringSerializer = new StringSerializer();
     private StringDeserializer stringDeserializer = new StringDeserializer();
     private LongDeserializer longDeserializer = new LongDeserializer();
 
@@ -47,56 +44,57 @@ class TimeWindowedCountTopologyTest extends TopologyTestBase {
 
     @Test
     public void testWindowedAggregation() {
-        var factory = new ConsumerRecordFactory<>(stringSerializer, stringSerializer);
+        var stringSerializer = new StringSerializer();
+        var inputTopic = testDriver.createInputTopic(INPUT_TOPIC, stringSerializer, stringSerializer);
 
         var start = Instant.parse("2019-04-20T10:35:00.00Z");
 
         // first window starts here
-        testDriver.pipeInput(factory.create(INPUT_TOPIC, "a", "1", start.toEpochMilli()));
-        testDriver.pipeInput(factory.create(INPUT_TOPIC, "a", "2", start.plus(1, ChronoUnit.MINUTES).toEpochMilli()));
-        testDriver.pipeInput(factory.create(INPUT_TOPIC, "a", "3", start.plus(2, ChronoUnit.MINUTES).toEpochMilli()));
-        testDriver.pipeInput(factory.create(INPUT_TOPIC, "a", "4", start.plus(3, ChronoUnit.MINUTES).toEpochMilli()));
-        testDriver.pipeInput(factory.create(INPUT_TOPIC, "a", "bad message", -1L));
+        inputTopic.pipeInput("a", "1", start.toEpochMilli());
+        inputTopic.pipeInput("a", "2", start.plus(1, ChronoUnit.MINUTES).toEpochMilli());
+        inputTopic.pipeInput("a", "3", start.plus(2, ChronoUnit.MINUTES).toEpochMilli());
+        inputTopic.pipeInput("a", "4", start.plus(3, ChronoUnit.MINUTES).toEpochMilli());
+
         // second window starts here
-        testDriver.pipeInput(factory.create(INPUT_TOPIC, "a", "5", start.plus(5, ChronoUnit.MINUTES).toEpochMilli()));
-        testDriver.pipeInput(factory.create(INPUT_TOPIC, "a", "6", start.plus(6, ChronoUnit.MINUTES).toEpochMilli()));
-        testDriver.pipeInput(factory.create(INPUT_TOPIC, "a", "7", start.plus(7, ChronoUnit.MINUTES).toEpochMilli()));
-        testDriver.pipeInput(factory.create(INPUT_TOPIC, "a", "8", start.plus(8, ChronoUnit.MINUTES).toEpochMilli()));
-        testDriver.pipeInput(factory.create(INPUT_TOPIC, "a", "9", start.plus(9, ChronoUnit.MINUTES).toEpochMilli()));
+        inputTopic.pipeInput("a", "5", start.plus(5, ChronoUnit.MINUTES).toEpochMilli());
+        inputTopic.pipeInput("a", "6", start.plus(6, ChronoUnit.MINUTES).toEpochMilli());
+        inputTopic.pipeInput("a", "7", start.plus(7, ChronoUnit.MINUTES).toEpochMilli());
+        inputTopic.pipeInput("a", "8", start.plus(8, ChronoUnit.MINUTES).toEpochMilli());
+        inputTopic.pipeInput("a", "9", start.plus(9, ChronoUnit.MINUTES).toEpochMilli());
 
-        testDriver.pipeInput(factory.create(INPUT_TOPIC, "a", "1 Day", start.plus(1, ChronoUnit.DAYS).toEpochMilli()));
+        inputTopic.pipeInput("a", "1 Day", start.plus(1, ChronoUnit.DAYS).toEpochMilli());
 
-        testDriver.pipeInput(factory.create(INPUT_TOPIC, "a", "11", start.plus(8, ChronoUnit.MINUTES).toEpochMilli()));
+        inputTopic.pipeInput("a", "11", start.plus(8, ChronoUnit.MINUTES).toEpochMilli());
 
-        testDriver.pipeInput(factory.create(INPUT_TOPIC, "a", "2 Days", start.plus(2, ChronoUnit.DAYS).toEpochMilli()));
+        inputTopic.pipeInput("a", "2 Days", start.plus(2, ChronoUnit.DAYS).toEpochMilli());
 
         // beyond the implicit grace period of 1 day
-        testDriver.pipeInput(factory.create(INPUT_TOPIC, "a", "12", start.plus(4, ChronoUnit.MINUTES).toEpochMilli()));
+        inputTopic.pipeInput("a", "12", start.plus(4, ChronoUnit.MINUTES).toEpochMilli());
 
+        var stringDeserializer = new StringDeserializer();
+        var longDeserializer = new LongDeserializer();
+        var outputTopic = testDriver.createOutputTopic(OUTPUT_TOPIC, stringDeserializer, longDeserializer);
 
-        OutputVerifier.compareValue(readNextRecord(), 1L);
-        OutputVerifier.compareValue(readNextRecord(), 2L);
-        OutputVerifier.compareValue(readNextRecord(), 3L);
-        OutputVerifier.compareValue(readNextRecord(), 4L);
+        assertThat(outputTopic.readValue()).isEqualTo(1L);
+        assertThat(outputTopic.readValue()).isEqualTo(2L);
+        assertThat(outputTopic.readValue()).isEqualTo(3L);
+        assertThat(outputTopic.readValue()).isEqualTo(4L);
 
-        OutputVerifier.compareValue(readNextRecord(), 1L);
-        OutputVerifier.compareValue(readNextRecord(), 2L);
-        OutputVerifier.compareValue(readNextRecord(), 3L);
-        OutputVerifier.compareValue(readNextRecord(), 4L);
-        OutputVerifier.compareValue(readNextRecord(), 5L);
+        assertThat(outputTopic.readValue()).isEqualTo(1L);
+        assertThat(outputTopic.readValue()).isEqualTo(2L);
+        assertThat(outputTopic.readValue()).isEqualTo(3L);
+        assertThat(outputTopic.readValue()).isEqualTo(4L);
+        assertThat(outputTopic.readValue()).isEqualTo(5L);
 
-        OutputVerifier.compareValue(readNextRecord(), 1L);
+        assertThat(outputTopic.readValue()).isEqualTo(1L);
 
         // Late arrival causes reissuing of record
-        OutputVerifier.compareValue(readNextRecord(), 6L);
+        assertThat(outputTopic.readValue()).isEqualTo(6L);
 
-        OutputVerifier.compareValue(readNextRecord(), 1L);
+        assertThat(outputTopic.readValue()).isEqualTo(1L);
 
         // No more records expected, window has expired
-        assertNull(readNextRecord());
+        assertThat(outputTopic.isEmpty()).isTrue();
     }
 
-    private ProducerRecord<String, Long> readNextRecord() {
-        return testDriver.readOutput(OUTPUT_TOPIC, stringDeserializer, longDeserializer);
-    }
 }
